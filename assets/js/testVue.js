8890c86828e00d0113934ec14280310c3af4036a
greyhub <luongnv.grey@gmail.com>
2020-10-25 12:48:19 +0700

    update template web

---
+++ assets/js/testVue.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    imageTest: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    linkTest: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+  } 
+})
